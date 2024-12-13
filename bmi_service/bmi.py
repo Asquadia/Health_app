69b3352ebf6287763dffc2a1a878e88c33be36c6
Asquadia <zineddine.y.berrichi@gmail.com>
2024-12-13 16:19:42 +0100

    Update bmi.py

--- bmi_service/bmi.py
+++ bmi_service/bmi.py
@@ -24,4 +24,4 @@ def calculate_bmi():
     return jsonify({'bmi': bmi_result})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5001)
