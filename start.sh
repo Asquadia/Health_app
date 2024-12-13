5a7bead4a060f5c943d520136fcd995842942aad
Asquadia <zineddine.y.berrichi@gmail.com>
2024-12-13 16:17:47 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
 source /app/venv/bin/activate
-gunicorn -w 4 -b 0.0.0.0:5000 backend.app:app &
-gunicorn -w 4 -b 0.0.0.0:5001 bmi_service.bmi:app &
-gunicorn -w 4 -b 0.0.0.0:5002 bmr_service.bmr:app &
+gunicorn -w 4 -b 0.0.0.0:5000 backend.app:app
 wait
